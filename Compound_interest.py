e6c5f3fb633b29b7d7a92db5c5ea0f3d5bba2271
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-04-27 15:54:51 +0530

    def compound_interest(principle, rate, time):
       Amount = principle * (pow((1 + rate / 100), time))
       print("%0.2f" %Amount)
    p,r,t=map(int,input().split())
    compound_interest(p,r,t)

---
+++ Compound_interest.py
@@ -0,0 +1,6 @@
+def compound_interest(principle, rate, time):
+ 
+   Amount = principle * (pow((1 + rate / 100), time))
+   print("%0.2f" %Amount)
+p,r,t=map(int,input().split())
+compound_interest(p,r,t)
