470bb6eeb1164612cfd0c0f28fbe03cc05476e88
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-05-16 17:49:47 +0530

    def hcf(a,b):     if(a>b):         c=a         while True:             if a%c==0 and b%c==0:                 return c             else:                 c-=1     else:         c=b         while True:             if a%c==0 and b%c==0:                 return c             else:                 c-=1
    a,b=map(int,input().split())
    c=hcf(a,b)
    print(c)
    def gcd(a,b):
        if(a%b==0):
            return b
        else:
            return gcd(b,a%b)

---
+++ GCD_or_HCF.py
@@ -0,0 +1,24 @@
+def hcf(a,b):
+    if(a>b):
+        c=a
+        while True:
+            if a%c==0 and b%c==0:
+                return c
+            else:
+                c-=1
+    else:
+        c=b
+        while True:
+            if a%c==0 and b%c==0:
+                return c
+            else:
+                c-=1
+        
+a,b=map(int,input().split())
+c=hcf(a,b)
+print(c)
+def gcd(a,b):
+    if(a%b==0):
+        return b
+    else:
+        return gcd(b,a%b)
