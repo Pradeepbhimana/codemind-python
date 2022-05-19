472d7d0ef0ce8ec24e4d4332d615c0cc4e38c12d
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-05-19 20:40:21 +0530

    a=input() b=len(a) c=0 for i in a:     if(i=='1'):         c+=1     elif(i=='2'):         c+=2     elif(i=='3'):         c+=3     elif(i=='4'):         c+=4     elif(i=='5'):         c+=5     elif(i=='6'):         c+=6     elif(i=='7'):         c+=7     elif(i=='8'):         c+=8     elif(i=='9'):         c+=9 print(c)

---
+++ Numbers_in_String.py
@@ -0,0 +1,25 @@
+
+
+a=input()
+b=len(a)
+c=0
+for i in a:
+    if(i=='1'):
+        c+=1
+    elif(i=='2'):
+        c+=2
+    elif(i=='3'):
+        c+=3
+    elif(i=='4'):
+        c+=4
+    elif(i=='5'):
+        c+=5
+    elif(i=='6'):
+        c+=6
+    elif(i=='7'):
+        c+=7
+    elif(i=='8'):
+        c+=8
+    elif(i=='9'):
+        c+=9
+print(c)
