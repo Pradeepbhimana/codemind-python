0927a402bb7a957a83305c1ecbd9d3ecfcac786c
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-06-02 19:33:50 +0530

    a=input() a=a.lower() if('a' in a and 'b' in a and 'c' in a and'd' in a and'e' in a and'f' in a and'g' in a and'h' in a and'i' in a and'j' in a and'k' in a and'l' in a and'm' in a and'n' in a and'o' in a and'p' in a and'q' in a and'r' in a and's' in a and't' in a and'u' in a and'v' in a and'w' in a and'x' in a and'y' in a and'z' in a ):     print(True) else:     print(False)

---
+++ Pangram_or_not.py
@@ -0,0 +1,6 @@
+a=input()
+a=a.lower()
+if('a' in a and 'b' in a and 'c' in a and'd' in a and'e' in a and'f' in a and'g' in a and'h' in a and'i' in a and'j' in a and'k' in a and'l' in a and'm' in a and'n' in a and'o' in a and'p' in a and'q' in a and'r' in a and's' in a and't' in a and'u' in a and'v' in a and'w' in a and'x' in a and'y' in a and'z' in a ):
+    print(True)
+else:
+    print(False)
