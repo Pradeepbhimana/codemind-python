815c25d8c542ff987395996400ea9309ba7a463b
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-06-02 19:55:32 +0530

    a=input()
    c=0
    
    if('a' in a  and 'e' in a  and 'i' in a  and 'o' in a  and 'u' in a):
        
        print(True)
    elif( 'A' in a  and 'E' in a  and 'I' in a  and 'O' in a  and 'U' in a):
        print(True)
    else:
        print(False)

---
+++ All_vowels_in_a_string.py
@@ -0,0 +1,11 @@
+a=input()
+
+c=0
+
+if('a' in a  and 'e' in a  and 'i' in a  and 'o' in a  and 'u' in a):
+    
+    print(True)
+elif( 'A' in a  and 'E' in a  and 'I' in a  and 'O' in a  and 'U' in a):
+    print(True)
+else:
+    print(False)
