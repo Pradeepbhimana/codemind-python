c6265574f0aacc1b5c8029f21103788f0a0a2c44
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-06-29 10:59:47 +0530

    def pr(a):     if(a<=1):         return 0     else:         for i in range(2,a):             if(a%i==0):                 return 0                 break         else:             return 1 a=int(input()) b=int(input()) for i in range(a,b+1):     if(pr(i)):         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -2,13 +2,14 @@ def pr(a):
     if(a<=1):
         return 0
     else:
-        for i in range (2,a):
+        for i in range(2,a):
             if(a%i==0):
                 return 0
+                break
         else:
             return 1
 a=int(input())
 b=int(input())
-for i in range (a,b+1):
+for i in range(a,b+1):
     if(pr(i)):
         print(i)
