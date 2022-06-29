4b16116541caa12cd01e6bd9eafcbab2c9ef1668
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-06-29 21:07:49 +0530

    a=int(input()) b=[] while(a):     b.append(a%10)     a//=10 print(max(b))

--- Largest_Digit.py
+++ Largest_Digit.py
@@ -1,16 +1,6 @@
 a=int(input())
-b=a
-c=0
-while(b!=0):
-    c+=1
-    b//=10
-d=[]
-b1=a
-for i in range(c):
-    d.append(b1%10)
-    b1//=10
-q=0
-for i in d:
-    if(i>q):
-        q=i
-print(q)
+b=[]
+while(a):
+    b.append(a%10)
+    a//=10
+print(max(b))
