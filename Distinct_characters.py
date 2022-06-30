76a83779e6c7420b14a001b6cfb4a7397e7089eb
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-06-30 17:18:29 +0530

    a=input() a=a.lower() b="" for i in a:     if i not in b and i!=' ':         b+=i b=sorted(b) for i in b:     print(i,end="")

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,18 +1,10 @@
 a=input()
 a=a.lower()
 b=""
-
-
 for i in a:
-    c=0
-    for j in a:
-        if(i==j and i!=' '):
-            c+=1
-    if(c==1):
+    if i not in b and i!=' ':
         b+=i
 b=sorted(b)
 for i in b:
     print(i,end="")
-
-    
     
