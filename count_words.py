630c311641f67f05ab234aec14cd5de3a0c6aacb
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-06-30 18:40:04 +0530

    a=input() a=a.lower() w=a.split(" ") c=0 q="aeiou" for j in w:     if(j[0] in q and j[-1] not in q):         c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,17 +1,11 @@
+
 a=input()
 a=a.lower()
 w=a.split(" ")
-ab=['a','e','i','o','u']
-af=0
-for i in w:
-    d=0
-    for j in i:
-        
-        if(d==0):
-            c=j
-        else:
-            e=j
-        d+=1
-    if(c in ab and e not in ab):
-        af+=1
-print(af)
+c=0
+q="aeiou"
+for j in w:
+    if(j[0] in q and j[-1] not in q):
+        c+=1
+print(c)
+   
