d066acbe0f12257a140fbd1a206514cab6d8ae34
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-06-30 17:49:51 +0530

    s=input() p=input() s=s.lower() p=p.lower()
    d=""
    for i in p:
        if i not in s and i.isalpha() and i not in d :
            d+=i
    for i in s:
        if i not in p and i.isalpha() and i not in d :
            d+=i
    d=sorted(d)
    
    print(len(d))

--- Non_common_characters.py
+++ Non_common_characters.py
@@ -4,14 +4,12 @@ s=s.lower()
 p=p.lower()
 
 d=""
-c=0
 for i in p:
     if i not in s and i.isalpha() and i not in d :
         d+=i
-        c+=1
 for i in s:
     if i not in p and i.isalpha() and i not in d :
         d+=i
-        c+=1
+d=sorted(d)
 
-print(c)
+print(len(d))
