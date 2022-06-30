37291e8754bc520989d8ebb7e1d3a91c2ada8db9
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-06-30 17:08:47 +0530

    a=input() a=a.lower() b=""
    for i in a:
        c=0
        for j in a:
            if(i==j and i!=' '):
                c+=1
        if(c==1):
            b+=i
    b=sorted(b)
    for i in b:
        print(i,end="")

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,19 +1,18 @@
-def sort(lst):
-      
-    return sorted(lst, key = lambda x: (isinstance(x, str), x))
 a=input()
 a=a.lower()
-d=0
-b=[]
+b=""
+
+
 for i in a:
     c=0
     for j in a:
         if(i==j and i!=' '):
             c+=1
     if(c==1):
-        b.append(i)
-        #print(i)
-        d+=1
-b=sort(b)
+        b+=i
+b=sorted(b)
 for i in b:
     print(i,end="")
+
+    
+    
