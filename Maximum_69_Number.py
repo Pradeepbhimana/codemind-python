7e33339f95b965a5595d12e9552846c938c96550
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-05 21:07:30 +0530

    a=int(input()) c=[] while(a):     c.append(a%10)     a//=10 #print(c) for i in range(len(c)-1,-1,-1):     if(c[i]==6):         c[i]=9         break for i in c[::-1]:     print(i,end="")

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,22 +1,13 @@
 a=int(input())
-b=a
-c=0
-while(b!=0):
-    c+=1
-    b//=10
-d=[]
-b1=a
-for i in range(c):
-    d.append(b1%10)
-    b1//=10
-for i in range (c-1,-1,-1):
-    if(d[i]==6):
-        d[i]=9
+c=[]
+while(a):
+    c.append(a%10)
+    a//=10
+#print(c)
+for i in range(len(c)-1,-1,-1):
+    if(c[i]==6):
+        c[i]=9
         break
-sum=0
-for i in d[::-1]:
-    if(i==0):
-        sum=sum*10+10
-    else:
-        sum=sum*10+i
-print(sum)
+for i in c[::-1]:
+    print(i,end="")
+    
