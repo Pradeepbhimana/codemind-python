00bdd4a7355a6c37854a52e0a1cb0dca2b7a4830
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-05 20:55:11 +0530

    a,b=map(int,input().split()) if(a>b):     a,b=b,a for i in range(a,0,-1):     if(a%i==0 and b%i==0):         print(i)         break

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,19 +1,7 @@
-def hcf(a,b):
-    if(a>b):
-        c=a
-        while True:
-            if a%c==0 and b%c==0:
-                return c
-            else:
-                c-=1
-    else:
-        c=b
-        while True:
-            if a%c==0 and b%c==0:
-                return c
-            else:
-                c-=1
-        
 a,b=map(int,input().split())
-c=hcf(a,b)
-print(c)
+if(a>b):
+    a,b=b,a
+for i in range(a,0,-1):
+    if(a%i==0 and b%i==0):
+        print(i)
+        break
