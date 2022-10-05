c7e9ed29b12360e56e130c86add9dc0991427b05
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-05 20:16:47 +0530

    a=int(input()) s=0 for i in range(1,a):     if(a%i==0):         s+=i if(s>a):     print(True) else:     print(False)

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,17 +1,9 @@
-def kk(a):
-    su=0
-    if(a<=1):
-        return 0
-    else:
-        for i in range(1,a):
-            if(a%i==0):
-                su+=i
-    if(su>a):
-        return 1
-    else:
-        return 0
 a=int(input())
-if(kk(a)):
-    print("True")
+s=0
+for i in range(1,a):
+    if(a%i==0):
+        s+=i
+if(s>a):
+    print(True)
 else:
-    print("False")
+    print(False)
