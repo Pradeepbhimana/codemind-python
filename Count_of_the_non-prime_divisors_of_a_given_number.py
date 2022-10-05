29cf557fd4455836ccb5487347753d591a255bec
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-05 12:07:13 +0530

    import math def prime(n):     for i in range(2,int(math.sqrt(n))+1):         if(n%i==0):             return 0     return 1 a=int(input()) c=0 for i in range(1,a+1):     if(a%i==0):         if(prime(i)==0):             c+=1 print(c+1)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,17 +1,14 @@
-def prime(a):
-    if(a<=1):
-        return 0
-    else:
-        for i in range (2,a):
-            if(a%i==0):
-                break
-        else:
-            return 1
+import math
+def prime(n):
+    for i in range(2,int(math.sqrt(n))+1):
+        if(n%i==0):
+            return 0
+    return 1
 a=int(input())
 c=0
-for i in range (1,a):
-    if(a%i==0 and prime(i)!=1):
-        c+=1
+for i in range(1,a+1):
+    if(a%i==0):
+        if(prime(i)==0):
+            c+=1
 print(c+1)
-        
     
