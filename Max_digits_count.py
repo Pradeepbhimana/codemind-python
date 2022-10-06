4d092aeeeb8aeefb21244822bf8ecec33d4444b0
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-06 11:40:40 +0530

    def co(a):     s=0     while(a):         s+=1         a//=10     return s s=0 b=int(input()) a=list(map(int,input().split())) for i in range(len(a)):     if(co(a[i])>s):         s=co(a[i]) #print(s) q=0 for i in range(len(a)):     if(co(a[i])==s):         q+=1 print(q)

---
+++ Max_digits_count.py
@@ -0,0 +1,20 @@
+def co(a):
+    s=0
+    while(a):
+        s+=1
+        a//=10
+    return s
+s=0
+b=int(input())
+a=list(map(int,input().split()))
+for i in range(len(a)):
+    if(co(a[i])>s):
+        s=co(a[i])
+#print(s)
+q=0
+for i in range(len(a)):
+    if(co(a[i])==s):
+        q+=1
+print(q)
+        
+    
