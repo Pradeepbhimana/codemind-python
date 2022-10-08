8427b8e2cb7eacdc66fa5e9e782b8e3def4421eb
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-08 10:05:23 +0530

    a=int(input()) l=list(map(int,input().split())) mi,ma=map(int,input().split()) q=100000000000000000000000000 e=0 for i in l:     if(i<mi or i>ma):         if(i<q):             e=1             q=i if(e==0):     print("-1") else:     print(q)

---
+++ minimum_not_between_A_and_B.py
@@ -0,0 +1,16 @@
+a=int(input())
+l=list(map(int,input().split()))
+mi,ma=map(int,input().split())
+q=100000000000000000000000000
+e=0
+for i in l:
+    if(i<mi or i>ma):
+        if(i<q):
+            e=1
+            q=i
+if(e==0):
+    print("-1")
+else:
+    print(q)
+        
+
