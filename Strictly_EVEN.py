4f50bb2208a908cd0ea19d5e685227b08d5d2323
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-08 11:37:46 +0530

    a=int(input()) l=list(map(int,input().split())) w=0 for i in range(a):     if(i%2!=0):         if(l[i]%2==0):             print(False)             w=1 if(w==0):     print(True)

---
+++ Strictly_EVEN.py
@@ -0,0 +1,11 @@
+a=int(input())
+l=list(map(int,input().split()))
+w=0
+for i in range(a):
+    if(i%2!=0):
+        if(l[i]%2==0):
+            print(False)
+            w=1
+if(w==0):
+    print(True)
+    
