bd46992d37a18bb1f23c747a35818933c0630f68
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-09 17:36:55 +0530

    a=input() b=a.split() q=10000000000000000000000000000000 for i in b:     c=0     for j in i:         if(j in "aeiouAEIOU"):             c+=1     if(c<q):         q=c d=0 for i in b:     c=0     for j in i:         if(j in "aeiouAEIOU"):             c+=1     if(c==q):         d+=1 print(d)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,19 @@
+a=input()
+b=a.split()
+q=10000000000000000000000000000000
+for i in b:
+    c=0
+    for j in i:
+        if(j in "aeiouAEIOU"):
+            c+=1
+    if(c<q):
+        q=c
+d=0
+for i in b:
+    c=0
+    for j in i:
+        if(j in "aeiouAEIOU"):
+            c+=1
+    if(c==q):
+        d+=1
+print(d)
