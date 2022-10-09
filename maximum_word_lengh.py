2522074f0b2293ed9b09147efd9d283afecc3716
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-09 10:23:20 +0530

    a=input() c=a.split() d=0 for i in c:     if(len(i)>d):         d=len(i) print(d)

--- maximum_word_lengh.py
+++ maximum_word_lengh.py
@@ -1,12 +1,7 @@
 a=input()
-w=a.split(" ")
+c=a.split()
 d=0
-for i in w:
-    if(d==0):
-        c=len(i)
-    else:
-        if(c<len(i)):
-            
-            c=len(i)
-    d+=1  
-print(c)
+for i in c:
+    if(len(i)>d):
+        d=len(i)
+print(d)
