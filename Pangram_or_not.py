167164e45fd8e71847433219dd8d2452cff9fe3c
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-09 17:43:12 +0530

    a=input() a=a.lower() q="abcdefghijklmnopqrstuvwxyz" b=0 for i in q:     if( i not in a  and i!=" "):         print(False)         b=1         break if(b==0):     print(True)     #print(sorted(a))

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,6 +1,12 @@
 a=input()
 a=a.lower()
-if('a' in a and 'b' in a and 'c' in a and'd' in a and'e' in a and'f' in a and'g' in a and'h' in a and'i' in a and'j' in a and'k' in a and'l' in a and'm' in a and'n' in a and'o' in a and'p' in a and'q' in a and'r' in a and's' in a and't' in a and'u' in a and'v' in a and'w' in a and'x' in a and'y' in a and'z' in a ):
+q="abcdefghijklmnopqrstuvwxyz"
+b=0
+for i in q:
+    if( i not in a  and i!=" "):
+        print(False)
+        b=1
+        break
+if(b==0):
     print(True)
-else:
-    print(False)
+    #print(sorted(a))
