d8dc1a7a6a5923b37cdbfadad5d8ef5e974f15cd
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-09 17:51:24 +0530

    a=input() a=a.lower() b="aeiou" c=0 for i in b:     if(i not in a):         c=1         print(i,end=" ") if(c==0):     print("0")

--- vowels_not_in_a_string.py
+++ vowels_not_in_a_string.py
@@ -1,29 +1,10 @@
 a=input()
 a=a.lower()
+b="aeiou"
 c=0
-
-if('a' not in a ):
-    print("a",end=' ')
-else:
-    c+=1
-if('e' not in a ):
-    print("e",end=' ')
-else:
-    c+=1
-    
-if('i' not in a ):
-    print("i",end=' ')
-else:
-    c+=1
-
-if('o' not in a ):
-    print("o",end=' ')
-else:
-    c+=1
-
-if('u' not in a ):
-    print("u",end=' ')
-else:
-    c+=1
-if(c==5):
+for i in b:
+    if(i not in a):
+        c=1
+        print(i,end=" ")
+if(c==0):
     print("0")
