d638ebf69b7960c284cc164949270e1918a80b7f
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-09 10:26:09 +0530

    a=input() c=a.split() d=0 for i in c:     for j in i[::-1]:         print(j,end="")     print(end=" ")

--- reverse_every_word.py
+++ reverse_every_word.py
@@ -1,12 +1,7 @@
-def reverseword(s): 
-   
-   w = s.split(" ")
-          
-          
-   nw = [i[::-1] for i in w]
-          
-   ns = " ".join(nw)
-   return ns
-
-s = input()
-print(reverseword(s))
+a=input()
+c=a.split()
+d=0
+for i in c:
+    for j in i[::-1]:
+        print(j,end="")
+    print(end=" ")
